f5a55487155966d54d33d227bac92ab7f230b1be
autonotary <harryalashari@gmail.com>
2026-06-11 20:42:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function sayHello() {
-    alert("Halo dari JavaScript!");
+body{
+    font-family: Arial, sans-serif;
+    background:#f4f4f4;
+}
+
+.container{
+    width:500px;
+    margin:50px auto;
+    background:white;
+    padding:20px;
+    border-radius:10px;
+    box-shadow:0 0 10px rgba(0,0,0,0.1);
+}
+
+button{
+    padding:10px 20px;
+    cursor:pointer;
 }
